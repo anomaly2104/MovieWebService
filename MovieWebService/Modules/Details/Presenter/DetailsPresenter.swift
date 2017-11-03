b6b8f2c69b7467fe646d4d4e8077b355aed95d52
Udit Agarwal <udita@amazon.com>
2017-11-04 04:18:46 +0530

    Add missing newline

--- MovieWebService/Modules/Details/Presenter/DetailsPresenter.swift
+++ MovieWebService/Modules/Details/Presenter/DetailsPresenter.swift
@@ -19,6 +19,7 @@ class DetailsPresenter: DetailsModuleInput, DetailsViewOutput, DetailsInteractor
     }
     
     // MARK: - DetailsInteractorOutput
+    
     func foundMovie(movie: Film?) {
         view.showDetailsForMovie(movie: movie)
     }
